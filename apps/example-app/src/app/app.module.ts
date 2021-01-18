0f599205e049b06ea418dbde7c3638fc2fadd0ef
phumoonlight <phumoonlight@hotmail.com>
2021-01-18 15:01:16 +0700

    learn: how nx dep-graph work

--- apps/example-app/src/app/app.module.ts
+++ apps/example-app/src/app/app.module.ts
@@ -3,12 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
+import { AuthModule } from '@athenaeum-nx/auth';
+import { ExampleLibModule } from '@athenaeum-nx/example-lib';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot([], { initialNavigation: 'enabled' }),
+    AuthModule,
+    ExampleLibModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- apps/first-app/src/app/app.module.ts
+++ apps/first-app/src/app/app.module.ts
@@ -2,10 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AuthModule } from '@athenaeum-nx/auth';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, AuthModule],
   providers: [],
   bootstrap: [AppComponent],
 })
